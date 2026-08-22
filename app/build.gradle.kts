c047a2513f58be256df14b156448a83fa5bbded1
koleq(NICOLEv3) <o.kurz@atlas.cz>
2026-08-22 15:35:07 +0200

    Version Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0"
+        versionName = "22.08.26"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
